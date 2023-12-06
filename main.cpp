1e30cf7014271b70f323e2e920b34c3486639868
Val <107081651+Val-Dim@users.noreply.github.com>
2023-12-06 23:57:14 +0200

    Fixed class teams and Added Conference

--- main.cpp
+++ main.cpp
@@ -1,124 +1,152 @@
 #include <iostream>
-#include <cstdlib>
 #include <fstream>
-#include <string>
+#include <cstdlib>
 #include <vector>
+
 using namespace std;
 
-class Players{
-    private:
-    string name,position;
+class Players {
+private:
+    string name;
     int points;
-    Players*arr;
-    const int size=5;
 
-    public:
-    Players(string& name/*,string& position,int points*/);
-    void setPoints(int points);
+public:
+    Players(string name, int points);
+    string getName();
     int getPoints();
-    void setNamePos(string& fileName);
-    void printAll();
 };
 
-Players::Players(string& name/*,string& position,int points*/){
-    this->name=name;
-    // this->position=position;
-    // this->points=points;
+Players::Players(string name, int points) {
+    this->name = name;
+    this->points = points;
 }
 
-void Players::setPoints(int points){
-    this->points=points;
+string Players::getName() {
+    return name;
 }
 
-int Players::getPoints(){
+int Players::getPoints() {
     return points;
 }
 
-void Players::setNamePos(string& fileName){
-    ifstream myfile(fileName);
-    myfile.open(fileName);
-    if(myfile.is_open()){
-        string line;
-        vector<string> names;
-        while(getline(myfile,line)){
-            names.push_back(line);
-        }
-        myfile.close();
-        if(!names.empty()){
-            for(int i=0;i<5;i++){
-                name=names[i];
-            }
-        }
-    }  
-}
+class Team {
+private:
+    string teamName;
+    const int size = 5;
+    int totalPoints = 0;
+    Players** player;
+    int wins;
 
-void Players::printAll(){
-    cout<<name<</*","<<position<<*/endl;
-    //cout<<"Points scored:"<<getPoints()<<"\n"<<endl;
-}
-
-class Team{
-    private:
-    string playerName,playerPosition;
-    Players**pl_array;
-    string teamName,file;
-    int plPoints;
-    const int size=5;
-    
-    public:
+public:
     Team(string teamName);
-    ~Team();
-    int playerPoints(int plPoints);
     string getTeamName();
     void createPlayer(string file);
+    int teamPoints();
+    int getWins();
     void displayTeam();
 };
 
-Team::Team(string teamName){
-    pl_array=new Players*[size];
-    this->teamName=teamName;
+Team::Team(string teamName) {
+    this->teamName = teamName;
+    this->wins=0;
 }
 
-// int Team::playerPoints(int plPoints){
-//     for (int i = 0; i < size; i++){
-//         pl_array[i]->setPoints(plPoints);
-//     }
-//     return plPoints;
-// }
-
-string Team::getTeamName(){
+string Team::getTeamName() {
     return teamName;
 }
 
-// void Team::createPlayer(string file){
-//     this->file=file;
-//     for(int i=0;i<size;i++){
-//         pl_array[i]=new Players(playerName);
-//     }
-// }
-
-void Team::displayTeam(){
-    cout<<getTeamName()<<endl;
-    for (int i=0;i<size;i++){
-        pl_array[i]->printAll();
+void Team::createPlayer(string file) {
+    ifstream myfile(file);
+    if (myfile.is_open()) {
+        string line;
+        vector<string> names;
+        while (getline(myfile, line)) {
+            names.push_back(line);
+        }
+        myfile.close();
+        if (!names.empty()) {
+            player = new Players*[size];
+            for (int i = 0; i < names.size(); ++i) {
+                player[i] = new Players(names[i], rand() % 31);
+            }
+        }
     }
 }
 
-Team::~Team(){
-    for (int i=0;i<size;i++){
-        delete pl_array[i];
+int Team::teamPoints() {
+    totalPoints = 0;
+    for (int i = 0; i < size; i++) {
+        totalPoints += player[i]->getPoints();
     }
-    delete[] pl_array;
+    return totalPoints;
 }
 
-int main(){
-    srand((unsigned) time(0));
-	int points = rand()%31;
+int Team::getWins(){
+    wins++;
+    return wins;
+}
+
+void Team::displayTeam() {
+    cout << "Team " << getTeamName() << "->Total points:" << teamPoints() << endl;
+    for (int i = 0; i < size; i++) {
+        cout << player[i]->getName() << "->";
+        cout << "Points scored:" << player[i]->getPoints() << endl;
+    }
+}
+
+class Conference {
+private:
+    vector<Team> teams;
+
+public:
+    Conference() {};
+    void getTeam(const Team& team);
+    void confGames();
+};
+
+void Conference::getTeam(const Team& team) {
+    teams.push_back(team);
+}
+
+void Conference::confGames() {
+    for(int i=0;i<teams.size();i++){
+        for(int j=i+1;j<teams.size();j++){
+            cout<<teams[i].getTeamName()<<" vs "<<teams[j].getTeamName()<<endl;
+            int pointsTeamI = teams[i].teamPoints();
+            int pointsTeamJ = teams[j].teamPoints();
+            if (pointsTeamI < pointsTeamJ) {
+                cout << teams[j].getTeamName() << " wins!" << endl;
+                teams[j].getWins();
+            } else if (pointsTeamI > pointsTeamJ) {
+                cout << teams[i].getTeamName() << " wins!" << endl;
+                teams[i].getWins();
+            }else{
+                cout<<"Tie"<<endl;
+            }
+        }
+            
+    }
+}
+
+
+int main() {
+    srand((unsigned)time(NULL));
 
     Team atlanta("Atlanta");
-    atlanta.createPlayer("D:\\C++ programs\\Baketball game\\atlanta.txt");
-    //atlanta.playerPoints(points);
-    atlanta.displayTeam();
-    atlanta.~Team();
+    Team dallas("Dallas");
+    Team denver("Denver");
+    dallas.createPlayer("dallas.txt");
+    atlanta.createPlayer("atlanta.txt");
+    denver.createPlayer("denver.txt");
+    // dallas.displayTeam();
+    // denver.displayTeam();
+    // atlanta.displayTeam();
+
+    Conference conf;
+    conf.getTeam(atlanta);
+    conf.getTeam(dallas);
+    conf.getTeam(denver);
+    conf.confGames();
+
     return 0;
 }
