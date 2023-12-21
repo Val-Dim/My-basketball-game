ea86a8f8b2997c807fdd9bcafd0613fcef176ec3
Val <107081651+Val-Dim@users.noreply.github.com>
2023-12-21 02:15:56 +0200

    Updated Conference class and added Tournaments class

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstdlib>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -32,19 +33,24 @@ int Players::getPoints() {
 class Team {
 private:
     string teamName;
-    const int size = 5;
-    int totalPoints = 0;
+    int size = 5;
+    mutable int totalPoints = 0;
     Players** player;
     int wins;
 
 public:
+    Team();
     Team(string teamName);
     string getTeamName();
     void createPlayer(string file);
-    int teamPoints();
+    int teamPoints()const;
+    void addWin(); 
     int getWins();
     void displayTeam();
 };
+
+Team::Team(){
+}
 
 Team::Team(string teamName) {
     this->teamName = teamName;
@@ -67,13 +73,13 @@ void Team::createPlayer(string file) {
         if (!names.empty()) {
             player = new Players*[size];
             for (int i = 0; i < names.size(); ++i) {
-                player[i] = new Players(names[i], rand() % 31);
+                player[i] = new Players(names[i], rand() % 51);
             }
         }
     }
 }
 
-int Team::teamPoints() {
+int Team::teamPoints() const{
     totalPoints = 0;
     for (int i = 0; i < size; i++) {
         totalPoints += player[i]->getPoints();
@@ -81,8 +87,11 @@ int Team::teamPoints() {
     return totalPoints;
 }
 
-int Team::getWins(){
+void Team::addWin(){
     wins++;
+}
+
+int Team::getWins(){
     return wins;
 }
 
@@ -100,12 +109,21 @@ private:
 
 public:
     Conference() {};
-    void getTeam(const Team& team);
+    void getTeam(Team& team);
+    void sortTeams(vector<Team>& teams);
     void confGames();
+    vector<Team> rankedTeams();
+    void ranklist();
 };
 
-void Conference::getTeam(const Team& team) {
+void Conference::getTeam(Team& team) {
     teams.push_back(team);
+}
+
+void Conference::sortTeams(vector<Team>& teams){
+    sort(teams.begin(), teams.end(), [](const Team& a, const Team& b) {
+        return a.teamPoints() > b.teamPoints();
+    });
 }
 
 void Conference::confGames() {
@@ -116,18 +134,52 @@ void Conference::confGames() {
             int pointsTeamJ = teams[j].teamPoints();
             if (pointsTeamI < pointsTeamJ) {
                 cout << teams[j].getTeamName() << " wins!" << endl;
-                teams[j].getWins();
+                teams[j].addWin();
             } else if (pointsTeamI > pointsTeamJ) {
                 cout << teams[i].getTeamName() << " wins!" << endl;
-                teams[i].getWins();
+                teams[i].addWin();
             }else{
                 cout<<"Tie"<<endl;
             }
-        }
-            
+        }       
+    }
+    //sortTeams(teams);
+}
+
+vector<Team> Conference::rankedTeams(){
+    vector<Team>t=teams;
+    sortTeams(t);
+    return t;
+}
+
+void Conference::ranklist(){
+    for(int i=0;i<teams.size();i++){
+        cout<<teams[i].getTeamName()<<"-Wins: "<<teams[i].getWins()<<endl;
     }
 }
 
+class Tournament:public Conference{
+    private:
+    vector<Team>topTeams;
+
+    public:
+    Tournament():Conference(){}
+    void setUp();
+    void playMatches();
+};
+
+void Tournament::setUp(){
+    vector<Team>foo=rankedTeams();
+
+    for(int i=0;i<min(4,foo.size());i++){
+        topTeams.push_back(foo[i]);
+    }
+}
+
+void Tournament::playMatches(){
+    int front=0;
+    int back=topTeams.size()-1;
+}
 
 int main() {
     srand((unsigned)time(NULL));
@@ -135,9 +187,13 @@ int main() {
     Team atlanta("Atlanta");
     Team dallas("Dallas");
     Team denver("Denver");
+    Team lakers("Lakers");
+    Team celtics("Celtics");
     dallas.createPlayer("dallas.txt");
     atlanta.createPlayer("atlanta.txt");
     denver.createPlayer("denver.txt");
+    lakers.createPlayer("lakers.txt");
+    celtics.createPlayer("celtics.txt");
     // dallas.displayTeam();
     // denver.displayTeam();
     // atlanta.displayTeam();
@@ -146,7 +202,10 @@ int main() {
     conf.getTeam(atlanta);
     conf.getTeam(dallas);
     conf.getTeam(denver);
+    conf.getTeam(lakers);
+    conf.getTeam(celtics);
     conf.confGames();
+    conf.ranklist();
 
     return 0;
 }
